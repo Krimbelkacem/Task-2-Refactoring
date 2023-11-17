5f52b8b139b13b37b38e3d7557ccf430bc5ed9db
unknown <krimbelkacem.hamnache@fgei.ummto.dz>
2023-11-17 10:23:18 +0100

    refactoring2

--- battery/spindler_battery.py
+++ battery/spindler_battery.py
@@ -1,5 +1,9 @@
 from battery.battery import Battery
-from utils import add_years_to_date
+
+
+def add_years_to_date(original_date, years_to_add):
+    result = original_date.replace(year=original_date.year + years_to_add)
+    return result
 
 
 class SpindlerBattery(Battery):
@@ -8,7 +12,8 @@ class SpindlerBattery(Battery):
         self.last_service_date = last_service_date
 
     def needs_service(self):
-        date_which_battery_should_be_serviced_by = add_years_to_date(self.last_service_date, 2)
+        date_which_battery_should_be_serviced_by = add_years_to_date(
+            self.last_service_date, 2)
         if date_which_battery_should_be_serviced_by < self.current_date:
             return True
         else:
